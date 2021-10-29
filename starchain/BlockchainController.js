921528bc25b558816bb21c60e632197c2521f2c5
will <contact@heueso.com>
2021-10-29 10:55:27 +0800

    test route

--- starchain/BlockchainController.js
+++ starchain/BlockchainController.js
@@ -16,6 +16,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.getHello();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -115,6 +116,12 @@ class BlockchainController {
             }
             
         });
+    }
+
+    getHello() {
+        this.app.get("/hello", async (req, res) => {
+            return res.status(200).json("hello world");
+        })
     }
 
 }
